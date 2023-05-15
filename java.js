384b9e6436d19715f86cde6b83fa45ab32c1e09f
Gluglamuch3982 <133580448+Gluglamuch3982@users.noreply.github.com>
2023-05-15 21:21:08 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+// alert("a")
+// let x = prompt("sd")
+function func() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    var result = num1 + num2;
+
+    document.getElementById("result").innerHTML = result;
+}
